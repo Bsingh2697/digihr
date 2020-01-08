b1a6becac47f32e2392aa1d288c69429ce768374
Bharat <singh2697.bs@gmail.com>
2020-01-08 11:41:29 +0530

    Updates

---
+++ precache-manifest.75f0d9d870a10e373007f25a1d70c076.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74dc41b5e2a049047881e3eea6698e55",
+    "url": "/digihr/index.html"
+  },
+  {
+    "revision": "bfe01812e9171ba015d3",
+    "url": "/digihr/static/css/2.e22ab21b.chunk.css"
+  },
+  {
+    "revision": "0956a058217d8b645ff6",
+    "url": "/digihr/static/css/main.426de25f.chunk.css"
+  },
+  {
+    "revision": "bfe01812e9171ba015d3",
+    "url": "/digihr/static/js/2.a2e5ea72.chunk.js"
+  },
+  {
+    "revision": "38cf0cd5d3ffba33ab21c40534b388a3",
+    "url": "/digihr/static/js/2.a2e5ea72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0956a058217d8b645ff6",
+    "url": "/digihr/static/js/main.a3d13bda.chunk.js"
+  },
+  {
+    "revision": "d7fd4ada851eab77df72",
+    "url": "/digihr/static/js/runtime-main.468e1cef.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/digihr/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/digihr/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/digihr/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/digihr/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/digihr/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/digihr/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/digihr/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/digihr/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/digihr/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/digihr/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/digihr/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/digihr/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/digihr/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/digihr/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/digihr/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/digihr/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/digihr/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/digihr/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/digihr/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/digihr/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/digihr/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/digihr/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "96ac6de68337fa63fe03372333dda45b",
+    "url": "/digihr/static/media/ff.96ac6de6.png"
+  },
+  {
+    "revision": "09b4b8028e3df05a1386c41005203457",
+    "url": "/digihr/static/media/img1.09b4b802.png"
+  },
+  {
+    "revision": "5724d824f7a62f3f8ba035d573aa099c",
+    "url": "/digihr/static/media/img2.5724d824.png"
+  },
+  {
+    "revision": "dd15df6c1b187435602bdc7076f45dc4",
+    "url": "/digihr/static/media/img3.dd15df6c.png"
+  },
+  {
+    "revision": "d3e3e8dc2f5c6630e2bf7344ac4ee812",
+    "url": "/digihr/static/media/innohr.d3e3e8dc.png"
+  },
+  {
+    "revision": "17736fa9c43ace6fd2eb62284ac38aef",
+    "url": "/digihr/static/media/log.17736fa9.jpg"
+  },
+  {
+    "revision": "b1d1e0f6e63236f7acd9598236235c4b",
+    "url": "/digihr/static/media/logo.b1d1e0f6.png"
+  }
+]);
